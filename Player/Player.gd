4b88f6d489717c665e3588e45a2c98442f9c3da4
Nilo Serafim Neto <nilold@gmail.com>
2020-09-05 18:54:01 -0300

    state machine and attack animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,12 +6,32 @@ const FRICTION = 400
 
 var velocity = Vector2.ZERO
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move(delta)
+		ROLL:
+			roll(delta)
+		ATTACK:
+			attack(delta)
+
+
+func move(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -25,16 +45,29 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK;
+
+func roll(delta):
+	pass
+
+func attack(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
 
 func animate(vector):
 	if vector != Vector2.ZERO:
 		animationState.travel("Run")
 		animationTree.set("parameters/Idle/blend_position", vector)
 		animationTree.set("parameters/Run/blend_position", vector)
+		animationTree.set("parameters/Attack/blend_position", vector)
 	else:
 		animationState.travel("Idle")
 
-
+func attack_animation_finished():
+	state = MOVE
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
