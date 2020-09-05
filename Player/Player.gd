c4afe9aab72e527c9da1f74300f2ad14e6358f6e
Nilo Serafim Neto <nilold@gmail.com>
2020-09-05 17:02:32 -0300

    Player run and idle animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,18 +6,33 @@ const FRICTION = 400
 
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
+	animate(input_vector)
+
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
+
+func animate(vector):
+	if vector != Vector2.ZERO:
+		animationState.travel("Run")
+		animationTree.set("parameters/Idle/blend_position", vector)
+		animationTree.set("parameters/Run/blend_position", vector)
+	else:
+		animationState.travel("Idle")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
